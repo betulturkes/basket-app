cf8fe097f335a478525b8d80194415be4707ca53
Betul Turkes <bbetulturkess@gmail.com>
2023-07-24 13:13:27 +0300

    Ürün fotoğrafları eklendi

--- src/App.js
+++ src/App.js
@@ -1,24 +1,74 @@
-import logo from './logo.svg';
+import {useState} from "react";
+import { Container,SimpleGrid,List, ThemeIcon, Input  } from '@mantine/core';
+import { IconCircleCheck, IconCircleDashed } from '@tabler/icons-react';
 import './App.css';
+import Card from "./components/Card";
+
+const storeItems= [{
+  name: "Sandalye",
+  src: "chair",
+  price:250
+},
+{
+  name: "Askı",
+  src: "hanger",
+  price:20
+},
+{
+  name: "Masa Lambası",
+  src: "lamp",
+  price:125
+},
+{
+  name: "Dekoratif Tabak",
+  src: "plate",
+  price:180
+},
+{
+  name: "Dekoratif Heykel",
+  src: "statue",
+  price:65
+},
+{
+  name: "Şarap Bardağı",
+  src: "wineglass",
+  price:40
+}];
+
+
+
 
 function App() {
+  let [basketItems,setBasketItems] = useState([]);
+  let [searchValue, setSearchValue] = useState(""); 
+  let filteredItems = basketItems.filter((item) => item.name.toLowerCase().indexOf(searchValue.toLowerCase()) >= 0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="App">
+      <SimpleGrid cols={3} className= "Store">
+        {storeItems.map(({name, src}) =>{
+          return <Card key={name} name={name} src={src} onAdd={() =>setBasketItems([...basketItems, {name}])}/>;
+
+        })}
+      </SimpleGrid >
+      <Input.Wrapper label="Arama">
+  <Input onChange = {(e) => setSearchValue(e.target.value)}/>
+</Input.Wrapper>
+      <List
+      className="List"
+      spacing="xs"
+      size="sm"
+      center
+      icon={
+        <ThemeIcon color="teal" size={24} radius="xl">
+          <IconCircleCheck size="1rem" />
+        </ThemeIcon>
+      }
+    >
+      {filteredItems.map(({name},index) =><List.Item key={index}>{name}</List.Item>)}
+      
+    </List>
+    </Container>
   );
 }
 
